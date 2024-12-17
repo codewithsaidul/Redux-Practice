7b83f1eb2ec68c763d269b016b2c5b499a1af5f1
codewithsaidul <codewithsaidul@gmail.com>
2024-12-17 12:48:24 +0600

    reducer function createed

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ const decrementEl = document.getElementById('decrement');
 const initialState = {
     count: 0,
 }
+
+
+// create a reducer function
+
+const counterReducer = (state = initialState, action) => {
+    if (action.type === 'increment') {
+        return { ...state, value: state.value + 1}
+    } else if (action.type === 'decrement') {
+        return {...state, value: state.value - 1}
+    } else {
+        return state
+    }
+}
