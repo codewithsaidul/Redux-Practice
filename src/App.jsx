8bc5edca34a9b2cf3c55acd87333adcaa018eddf
codewithsaidul <codewithsaidul@gmail.com>
2025-03-14 23:10:49 +0600

    redux store setup completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
 // import { useState } from "react";
+import { Provider } from "react-redux";
 import Counter from "./counter/Counter";
+import store from "./redux/store";
 
 
 function App() {
   // const [count, setCount] = useState(0);
 
   return (
-    <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
-      <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
-        Simple Counter Application
-      </h1>
-
-      <div className="max-w-md mx-auto mt-10 space-y-5">
-        <Counter />
-        <Counter />
-        <Counter />
+    <Provider store={store}>
+      <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
+        <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
+          Simple Counter Application
+        </h1>
+  
+        <div className="max-w-md mx-auto mt-10 space-y-5">
+          <Counter />
+          <Counter />
+          <Counter />
+        </div>
       </div>
-    </div>
+    </Provider>
   );
 }
 
