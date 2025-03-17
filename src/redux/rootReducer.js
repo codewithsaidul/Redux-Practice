17e8e960c0cea6576001396eb06c7a03e45f6e32
codewithsaidul <codewithsaidul@gmail.com>
2025-03-18 00:05:28 +0600

    todo & filter reducer added on redux store

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,11 +1,15 @@
 import { combineReducers } from "redux";
 import counterReducer from "./counter/counterReducer";
 import dynamicReducer from "./dynamicCounter/dynamicReducer";
+import filterReducer from "./SimpleTodo/filter/filterReducer";
+import todoReducer from "./SimpleTodo/todo/todoReducer";
 
 
 const rootReducer = combineReducers({
     counter: counterReducer,
-    dynamic: dynamicReducer
+    dynamic: dynamicReducer,
+    todos: todoReducer,
+    todoFilter: filterReducer
 })
 
 
