104ea03932bb89c2b0602f4b5e0f9a95614ab661
codewithsaidul <codewithsaidul@gmail.com>
2025-04-03 22:27:07 +0600

    new branch setup

--- reduxbunny/src/redux/features/video/videoApi.js
+++
@@ -1,9 +0,0 @@
-import axios from "../../../utils/axios";
-
-const fetchVideo = async (id) => {
-  const response = await axios.get(`/videos/${id}`);
-
-  return response.data;
-};
-
-export default fetchVideo;
