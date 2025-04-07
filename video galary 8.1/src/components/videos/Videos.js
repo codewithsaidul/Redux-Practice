8146988c4f64a6dc8b6dc99d55d6924550341701
codewithsaidul <codewithsaidul@gmail.com>
2025-04-07 12:47:30 +0600

    implement fetch single video & related video data

--- video galary 8.1/src/components/videos/Videos.js
+++ video galary 8.1/src/components/videos/Videos.js
@@ -1,15 +1,43 @@
+import { useGetVideosQuery } from "../../feature/api/apiSlice";
+import Error from "../ui/Error";
+import VideoLoader from "../ui/loaders/VideoLoader";
 import Video from "./Video";
 
 export default function Videos() {
-    return (
-        <>
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-        </>
+  const { data: videos, isLoading, isError } = useGetVideosQuery();
+
+  // decide what to render
+  let content;
+
+  if (isLoading) {
+    content = (
+      <>
+        <VideoLoader />
+        <VideoLoader />
+        <VideoLoader />
+        <VideoLoader />
+        <VideoLoader />
+        <VideoLoader />
+        <VideoLoader />
+        <VideoLoader />
+        <VideoLoader />
+        <VideoLoader />
+      </>
     );
+  }
+
+  if (!isLoading && isError) {
+    content = <Error message={"There was an error occured!"} />
+  }
+
+  if (!isLoading && !isError && videos.length === 0) {
+    content = <Error message={"No Videos Found!"} />
+  }
+
+
+  if (!isLoading && !isError && videos.length !== 0) {
+    content = videos?.map((video) => <Video key={video.id} video={video} />);
+  }
+
+  return <>{content}</>;
 }
--- video galary 8.1/src/feature/api/apiSlice.js
+++ video galary 8.1/src/feature/api/apiSlice.js
@@ -1,4 +1,4 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 
 export const apiSlice = createApi({
@@ -6,5 +6,24 @@ export const apiSlice = createApi({
     baseQuery: fetchBaseQuery({
         baseUrl: "http://localhost:9000"
     }),
-    endpoints: (builder) => ({})
+    endpoints: (builder) => ({
+        getVideos: builder.query({
+            query: () => "/videos"
+        }),
+        getVideo: builder.query({
+            query: (id) => `/videos/${id}`
+        }),
+        // ?title_like=css
+        getRelatedVideos: builder.query({
+            query: (title) => {
+                const tags = title.split(" ")
+                const likes = tags.map(tag => `title_like=${tag}`);
+                const queryString = `/videos?${likes.join("&")}&_limit=3`;
+                return queryString
+            }
+        })
+    })
 })
+
+
+export const { useGetVideosQuery, useGetVideoQuery, useGetRelatedVideosQuery } = apiSlice
